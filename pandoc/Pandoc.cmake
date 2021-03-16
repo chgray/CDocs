7c69d081e1ef6866c12f7356df9a0445d1838939
Chris Gray (COSINE FUNDAMENTALS) <chgray@microsoft.com>
2021-03-15 20:19:18 -0700

    working on docker

--- pandoc/Pandoc.cmake
+++ pandoc/Pandoc.cmake
@@ -11,30 +11,30 @@ function(GENERATE_PANDOC_SLIDES)
     foreach(arg IN LISTS ARGV)
         get_filename_component(RAW_FILENAME ${arg} NAME)
 
-        set(HTML_HASH_FILE ${RAW_FILENAME}.slides.html)
-        set(HTML_OUTPUT_FILE ${PANDOC_DEST_DIR}/${arg}.slides.html)
-        get_filename_component(OUTPUT_DIR ${HTML_OUTPUT_FILE} DIRECTORY)
+        set(HASH_FILE ${RAW_FILENAME}.slides.html)
+        set(OUTPUT_FILE ${PANDOC_DEST_DIR}/${arg}.slides.html)
+        get_filename_component(OUTPUT_DIR ${OUTPUT_FILE} DIRECTORY)
 
 
-        get_filename_component(OUTPUT_DIR ${HTML_OUTPUT_FILE} DIRECTORY)
+        get_filename_component(OUTPUT_DIR ${OUTPUT_FILE} DIRECTORY)
 
-        message(STATUS "OutputFile : ${HTML_OUTPUT_FILE} coming from ${arg}")
+        message(STATUS "OutputFile : ${OUTPUT_FILE} coming from ${arg}")
         message(STATUS "OutputDir : ${OUTPUT_DIR}")
 
         add_custom_command(
-            OUTPUT ${HTML_HASH_FILE}
+            OUTPUT ${HASH_FILE}
             DEPENDS ${arg}
 
             WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
 
-            COMMENT "pandoc -t slidy --self-contained -s ${arg} -o ${HTML_OUTPUT_FILE} --filter pandoc-plot --lua-filter /lua-filters/include-files.lua -F mermaid-filter --filter pandoc-plantuml"
+            COMMENT "pandoc -t slidy --self-contained -s ${arg} -o ${OUTPUT_FILE} --filter pandoc-plot --lua-filter /lua-filters/include-files.lua -F mermaid-filter --filter pandoc-plantuml"
 
             COMMAND mkdir -p ${OUTPUT_DIR}
-            COMMAND pandoc -t slidy --self-contained -s ${arg} -o ${HTML_OUTPUT_FILE} --filter pandoc-plot --lua-filter /lua-filters/include-files.lua -F mermaid-filter --filter pandoc-plantuml
+            COMMAND pandoc -t slidy --self-contained -s ${arg} -o ${OUTPUT_FILE} --filter pandoc-plot --lua-filter /lua-filters/include-files.lua -F mermaid-filter --filter pandoc-plantuml
         )
 
-        message(STATUS ">>>>>>> Pandoc Source File = ${RAW_FILENAME} output = ${HTML_HASH_FILE}")
-        list(APPEND pandoc_output_files ${HTML_HASH_FILE})
+        message(STATUS ">>>>>>> Pandoc Source File = ${RAW_FILENAME} output = ${HASH_FILE}")
+        list(APPEND pandoc_output_files ${HASH_FILE})
     endforeach()
 
     message(STATUS "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
@@ -54,27 +54,67 @@ function(GENERATE_PDFS)
     foreach(arg IN LISTS ARGV)
         get_filename_component(RAW_FILENAME ${arg} NAME)
 
-        set(HTML_HASH_FILE ${RAW_FILENAME}.pdf)
-        set(HTML_OUTPUT_FILE ${PANDOC_DEST_DIR}/${arg}.pdf)
-        get_filename_component(OUTPUT_DIR ${HTML_OUTPUT_FILE} DIRECTORY)
+        set(HASH_FILE ${RAW_FILENAME}.pdf)
+        set(OUTPUT_FILE ${PANDOC_DEST_DIR}/${arg}.pdf)
+        get_filename_component(OUTPUT_DIR ${OUTPUT_FILE} DIRECTORY)
 
-        message(STATUS "OutputFile : ${HTML_OUTPUT_FILE} coming from ${arg}")
+        message(STATUS "OutputFile : ${OUTPUT_FILE} coming from ${arg}")
         message(STATUS "OutputDir : ${OUTPUT_DIR}")
 
         add_custom_command(
-            OUTPUT ${HTML_HASH_FILE}
+            OUTPUT ${HASH_FILE}
             DEPENDS ${arg}
 
             WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
 
-            COMMENT "pandoc  --self-contained -s ./${arg} -o ${HTML_OUTPUT_FILE} --filter pandoc-plot --lua-filter /lua-filters/include-files.lua -F mermaid-filter"
+            COMMENT "pandoc  --self-contained -s ./${arg} -o ${OUTPUT_FILE} --filter pandoc-plot --lua-filter /lua-filters/include-files.lua -F mermaid-filter"
 
             COMMAND mkdir -p ${OUTPUT_DIR}
-            COMMAND pandoc  --self-contained -s ./${arg} -o ${HTML_OUTPUT_FILE} --filter pandoc-plot --lua-filter /lua-filters/include-files.lua -F mermaid-filter
+            COMMAND pandoc  --self-contained -s ./${arg} -o ${OUTPUT_FILE} --filter pandoc-plot --lua-filter /lua-filters/include-files.lua -F mermaid-filter
         )
 
-        message(STATUS ">>>>>>> Pandoc Source File = ${RAW_FILENAME} output = ${HTML_HASH_FILE}")
-        list(APPEND pandoc_output_files ${HTML_HASH_FILE})
+        message(STATUS ">>>>>>> Pandoc Source File = ${RAW_FILENAME} output = ${HASH_FILE}")
+        list(APPEND pandoc_output_files ${HASH_FILE})
+    endforeach()
+
+    message(STATUS "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+    message(STATUS "pandoc outputs = ${pandoc_output_files}")
+    add_custom_target(${library} DEPENDS ${pandoc_output_files})
+endfunction()
+
+
+function(GENERATE_WORD)
+    set(library ${ARGV0})
+    set(library_type ${ARGV1})
+    list(REMOVE_AT ARGV 0)
+    list(REMOVE_AT ARGV 0)
+
+    message(STATUS "****** : ${PANDOC_DEST_DIR}")
+
+    foreach(arg IN LISTS ARGV)
+        get_filename_component(RAW_FILENAME ${arg} NAME)
+
+        set(HASH_FILE ${RAW_FILENAME}.docx)
+        set(OUTPUT_FILE ${PANDOC_DEST_DIR}/${arg}.docx)
+        get_filename_component(OUTPUT_DIR ${OUTPUT_FILE} DIRECTORY)
+
+        message(STATUS "OutputFile : ${OUTPUT_FILE} coming from ${arg}")
+        message(STATUS "OutputDir : ${OUTPUT_DIR}")
+
+        add_custom_command(
+            OUTPUT ${HASH_FILE}
+            DEPENDS ${arg}
+
+            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+
+            COMMENT "pandoc  --self-contained -s ./${arg} -o ${OUTPUT_FILE} --filter pandoc-plot --lua-filter /lua-filters/include-files.lua -F mermaid-filter"
+
+            COMMAND mkdir -p ${OUTPUT_DIR}
+            COMMAND pandoc  --self-contained -s ./${arg} -o ${OUTPUT_FILE} --filter pandoc-plot --lua-filter /lua-filters/include-files.lua -F mermaid-filter
+        )
+
+        message(STATUS ">>>>>>> Pandoc Source File = ${RAW_FILENAME} output = ${HASH_FILE}")
+        list(APPEND pandoc_output_files ${HASH_FILE})
     endforeach()
 
     message(STATUS "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
