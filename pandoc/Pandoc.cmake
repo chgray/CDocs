fe123695eb4435537fd054eaaa6736a927113f99
Chris Gray (COSINE FUNDAMENTALS) <chgray@microsoft.com>
2021-04-29 18:29:42 -0700

    add wiki output

--- pandoc/Pandoc.cmake
+++ pandoc/Pandoc.cmake
@@ -83,6 +83,45 @@ function(GENERATE_PDFS)
 endfunction()
 
 
+function(GENERATE_WIKI)
+    set(library ${ARGV0})
+    set(library_type ${ARGV1})
+    list(REMOVE_AT ARGV 0)
+    list(REMOVE_AT ARGV 0)
+
+    message(STATUS "****** : ${PANDOC_DEST_DIR}")
+
+    foreach(arg IN LISTS ARGV)
+        get_filename_component(RAW_FILENAME ${arg} NAME)
+
+        set(HASH_FILE ${RAW_FILENAME}.wiki)
+        set(OUTPUT_FILE ${PANDOC_DEST_DIR}/${arg}.wiki)
+        get_filename_component(OUTPUT_DIR ${OUTPUT_FILE} DIRECTORY)
+
+        message(STATUS "OutputFile : ${OUTPUT_FILE} coming from ${arg}")
+        message(STATUS "OutputDir : ${OUTPUT_DIR}")
+
+        add_custom_command(
+            OUTPUT ${HASH_FILE}
+            DEPENDS ${arg}
+
+            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+
+            COMMENT "pandoc  -s ./${arg} -t mediawiki -o ${OUTPUT_FILE}"
+
+            COMMAND mkdir -p ${OUTPUT_DIR}
+            COMMAND pandoc  --self-contained -s ./${arg} -o ${OUTPUT_FILE} --filter pandoc-plot --lua-filter /lua-filters/include-files.lua -F mermaid-filter
+        )
+
+        message(STATUS ">>>>>>> Pandoc Source File = ${RAW_FILENAME} output = ${HASH_FILE}")
+        list(APPEND pandoc_output_files ${HASH_FILE})
+    endforeach()
+
+    message(STATUS "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+    message(STATUS "pandoc outputs = ${pandoc_output_files}")
+    add_custom_target(${library} DEPENDS ${pandoc_output_files})
+endfunction()
+
 function(GENERATE_WORD)
     set(library ${ARGV0})
     set(library_type ${ARGV1})
